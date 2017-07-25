992ff1b844f430867a057e31cb0a581efadd81ed
Henri Qiu <qiu.henri@gmail.com>
2017-07-26 01:02:33 +0200

    Card and CardSection

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {Text} from 'react-native';
+import Card from './Card';
+import CardSection from './CardSection';
 
 /* Presentational : functional component. No need
 to access state */
 
 const AlbumDetail = (props) => {
   return(
-    <View>
-      <Text>{props.album.title}</Text>
-    </View>
+    <Card>
+      <CardSection>
+          <Text>{props.album.title}</Text>
+      </CardSection>
+    </Card>
   );
 };
 
